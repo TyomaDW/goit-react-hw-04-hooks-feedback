26e948e0f6531069068ad9c471892f390cf2524b
Tyoma <risendrums777@gmail.com>
2022-02-01 21:44:03 +0200

    add

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import Section from './Section';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+
+function Feedback() {
+  const [feedbackState, setfeedbackState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const { good, neutral, bad } = feedbackState;
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) / 100;
+  };
+
+  const leaveFeedbackHandle = name => {
+    setfeedbackState(prevstate => ({
+      ...prevstate,
+      [name]: prevstate[name] + 1,
+    }));
+  };
+
+  const element = !countTotalFeedback() ? (
+    <Notification message="There is no feedback" />
+  ) : (
+    <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      total={countTotalFeedback()}
+      percentage={countPositiveFeedbackPercentage()}
+    />
+  );
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedbackState)}
+          onLeaveFeedback={leaveFeedbackHandle}
+        />
+      </Section>
+      <Section title="Statistics">{element}</Section>
+    </>
+  );
+}
+
+export default Feedback;
+
+Feedback.propTypes = {
+  good: PropTypes.string,
+  neutral: PropTypes.string,
+  bad: PropTypes.string,
+};
---
+++ src/components/Feedback/Statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+
+import styles from './Statistics.module.css';
+
+const Statistics = ({ good, neutral, bad, percentage, total }) => {
+  return (
+    <>
+      <p>
+        <span className={styles.spanText}>Good: </span>
+        {good}
+      </p>
+      <p>
+        <span className={styles.spanText}>Neutral: </span>
+        {neutral}
+      </p>
+      <p>
+        <span className={styles.spanText}>Bad: </span>
+        {bad}
+      </p>
+      <p>
+        <span className={styles.spanText}>Total: </span>
+        {total}
+      </p>
+      <p>
+        <span className={styles.spanText}>Positive feedback: </span>
+        {percentage}
+      </p>
+    </>
+  );
+};
+
+export default Statistics;
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  percentage: PropTypes.number,
+  total: PropTypes.number,
+};
