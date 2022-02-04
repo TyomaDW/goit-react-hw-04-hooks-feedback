f7bd36064783e2cf7c3d33f8e0b8c4789a4ef696
Tyoma <risendrums777@gmail.com>
2022-02-04 16:47:21 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import Feedback from './components/Feedback';
-
-import styles from './styles/App.module.css';
+import { useState } from 'react';
+import './App.css';
+import Section from './components/Section';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = { good, neutral, bad };
+
+  const onLeaveFeedback = option => {
+    switch (option.currentTarget.textContent) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    let positivePercentage = 0;
+
+    if (good !== 0 && bad !== 0) {
+      positivePercentage = (good / (bad + good)) * 100;
+    } else if (bad === 0 && good > 0) {
+      positivePercentage = 100;
+    }
+    return Math.round(positivePercentage * 100) / 100;
+  };
+
   return (
-    <div className={styles.container}>
-      <Feedback />
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </>
   );
 }
 
